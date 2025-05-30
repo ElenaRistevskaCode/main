f68c3f15cfb96e523967b663b44856ab32ee7ab2
Elena Ristevska <elena.ristevska@emitknowledge.com>
2025-05-30 14:38:18 +0200

    changes to function

--- project/test.js
+++ project/test.js
@@ -1,9 +1,9 @@
   function greetUser(name) {
     return `Hello, ${name}. Glad you're here user.`;
   }
-  
+
   function greetAdmin() {
-    return `Hello, admin. Glad you're here. Edited this line from from branch` ;
+    return `Hello, admin. Glad you're here. Welcome to the app.` ;
   }
 
   function calculateTotal(price, taxRate) {
