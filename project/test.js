aef556154a21f96ee094da69aafba09d209bbe84
Elena Ristevska <elena.ristevska@emitknowledge.com>
2025-05-30 13:28:45 +0200

    cacl function

--- project/test.js
+++ project/test.js
@@ -1,7 +1,11 @@
-function greetUser(name) {
+  function greetUser(name) {
     return `Hello, ${name}. Glad you're here user.`;
   }
+  
   function greetAdmin() {
     return `Hello, admin. Glad you're here. Edited this text from branch` ;
   }
-    
+
+  function calculateTotal(price, taxRate) {
+    return price + (price * taxRate);
+  }
