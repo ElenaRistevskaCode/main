ea2c6490eb82fd16efb6eabfdb3fc584c6411e0f
Elena Ristevska <elena.ristevska@emitknowledge.com>
2025-05-30 13:19:39 +0200

    edited

--- project/test.js
+++ project/test.js
@@ -2,6 +2,6 @@ function greetUser(name) {
     return `Hello, ${name}. Glad you're here.`;
   }
   function greetAdmin() {
-    return `Hello, admin. Glad you're here.`;
+    return `Hello, admin. Glad you're here. Edited from branch` ;
   }
     
